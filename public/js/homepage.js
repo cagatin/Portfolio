b45a65cc6a6917fb06302202bcfceeccc7150334
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-21 14:12:07 -0700

    Created fade scrolling effect for homepage

---
+++ public/js/homepage.js
@@ -0,0 +1,18 @@
+const reveal = function () {
+    const elements = document.querySelectorAll('.reveal');
+
+    for (let i = 0; i < elements.length; i++) {
+        let windowHeight = window.innerHeight;
+        let revealTop = elements[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            elements[i].classList.add('active');
+        }
+        else {
+            elements[i].classList.remove('active')
+        }
+    }
+}
+
+window.addEventListener('scroll', reveal);
