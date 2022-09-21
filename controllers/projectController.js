a57763a63cf4716dcd0b44bd985e47efb1b139d3
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-21 08:30:02 -0700

    Created GET controllers

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,1 +1,29 @@
 const Project = require('../models/Project');
+
+// Retrieve all available projects
+const getAllProjects = async function (req, res) {
+    try {
+        const projectData = await Project.find({});
+        if (!projectData) {
+            res.status(404).json({ message: "No Projects in the Database" });
+        }
+        res.status(200).json(projectData);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+// Retrieve single project based on _id route parameter
+const getSingleProject = async function (req, res) {
+    try {
+        const projectData = await Project.findById(req.params.id);
+        if (!projectData) {
+            res.status(404).json({ message: "No project exists with that ID" });
+        }
+        res.status(200).json(projectData);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+}
