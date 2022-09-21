d86d4118d0e256f6582155422e336a032b47ca94
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-21 16:36:30 -0700

    Removed residual database imports from server file

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 
 // importing connection and routes files
-const db = require('./config/connection');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
@@ -24,9 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));        //serve static f
 // Routes
 app.use(routes);
 
-// Start the server and open the connection
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-    });
+
+app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
 });
