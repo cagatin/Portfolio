f2711a1b4c6adfddc45ae421b6a79f227828e055
Felix Cagatin <cagatingilbert@gmail.com>
2022-09-21 16:35:14 -0700

    Removed mongo database

--- routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const projectRoutes = require('./projectRoutes');
-
-router.use('/project', projectRoutes);
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 //create route for home
 router.use('/', homeRoutes);
-
-//attatch api path to api routes
-router.use('/api', apiRoutes);
 
 module.exports = router;
